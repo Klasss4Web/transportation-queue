38cfaa0928e6ea54b5beb5be13043cefd1d2c8ab
OCHADE EMMANUEL <eochade16@gmail.com>
2021-05-04 15:41:55 +0100

    added favicon, nav bar, google font and company logo

--- index.js
+++ index.js
@@ -6,25 +6,16 @@ const callNext = document.querySelector('.call-next');
 
 let date = document.querySelector('.date');
 
-// console.log(customerId)
-
 //add event listener to the call-next button
 callNext.addEventListener('click', moveToPlanner)
 
 function moveToPlanner() {
   for(let i = 0; i < slot.length; i++) {
-    // console.log(customerName[i].innerHTML)
-    for(let j = 0; j < customerName.length; j++) {
-      // console.log(slot[i]) 
+    for(let j = 0; j < customerName.length; j++) { 
       }
       if(slot[i].innerHTML == "") {
         slot[i].innerHTML = customerName[i].innerHTML;
         break;
-        // if(slot[i].innerHTML !== customerName[0].innerHTML){
-        //    slot[i].innerHTML = customerName[0].innerHTML
-        // break;
-        // }
     }
   }
-  // console.log(customerName)
 }
